757ad1bb0a472f0bfc5103dba1cb2672ba18ebdf
Sai Gopi Narimeti <narimetisaigopi@gmail.com>
2024-01-02 09:11:36 +0530

    init

---
+++ lib/flutter_batch_2024_jan.dart
@@ -0,0 +1,7 @@
+int calculate() {
+  return 6 * 7;
+}
+
+main() {
+  print("i am another main");
+}
---
+++ test/flutter_batch_2024_jan_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_batch_2024_jan/flutter_batch_2024_jan.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
