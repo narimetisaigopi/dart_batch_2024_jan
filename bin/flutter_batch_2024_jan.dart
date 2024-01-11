49140af56ee58c30f24dce0b8b17b6c168b0e7b1
Sai Gopi Narimeti <narimetisaigopi@gmail.com>
2024-01-11 07:57:49 +0530

    sa

--- bin/flutter_batch_2024_jan.dart
+++ bin/flutter_batch_2024_jan.dart
@@ -4,9 +4,8 @@ void main() {
   // number
   int mrp = 34;
   double price = 34.0100000000000015; // float
-  //.56 * 1000000 = 
+  //.56 * 1000000 =
   bool isSub = false;
   //String age = "27";
-  print('Hello world and happy new year 2024');
-  
+  print('Hello world and happy new year');
 }
